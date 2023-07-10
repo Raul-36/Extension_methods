ecf12851b5250f7e31615d5b2af359fdf599e2d5
Rau-36 <raul363636r36d@gmail.com>
2023-07-10 20:27:00 +0400

    Add Direction

---
+++ Extension methods/Extension methods/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_methods
+{
+	public enum Direction
+    {
+        Ascending,
+        Descending
+    }
+}
